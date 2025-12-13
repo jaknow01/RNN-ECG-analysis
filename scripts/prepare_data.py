6eaf408ae33db739e7214b07d1110547171cb667
kuba <jakub.nowakowski01@onet.pl>
2025-12-13 14:41:50 +0100

    baseline visualizations

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -4,6 +4,7 @@ from typing import List
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler, SMOTE
 
 
 def download_data() -> pd.DataFrame:
@@ -11,12 +12,23 @@ def download_data() -> pd.DataFrame:
     return pd.read_csv(f"{path}/INCART 2-lead Arrhythmia Database.csv")
 
 def preproces_baseline_forest(df: pd.DataFrame):
+
+    #drop unneccessary columns
     df = df.drop('record', axis = 1)
     df = df.dropna()
-    df["type"] = df["type"].map({'N': 0, 'VEB': 1, 'SVEB': 2, 'F': 3, 'Q': 4})
+    df = df[df["type"] != 'Q']
+
+    df["type"] = df["type"].map({'N': 0, 'VEB': 1, 'SVEB': 2, 'F': 3})
+
+    #remove insignificant class
 
     X = df.drop('type', axis = 1)
     Y = df['type']
 
-    return train_test_split(X, Y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+    ros = SMOTE()
+    X_train_resampled, Y_train_resampled = ros.fit_resample(X_train, y_train)
+
+    return X_train_resampled, X_test, Y_train_resampled, y_test
 
